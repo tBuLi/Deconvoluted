0ebc758f177ba331b92258e5e03a200b9c506ef1
Martin Roelfs <u0114255@kuleuven.be>
2019-06-05 12:39:41 +0200

    Bump version: 0.1.0 → 0.1.1

--- deconvoluted/__init__.py
+++ deconvoluted/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Martin Roelfs"""
 __email__ = 'martin.roelfs@kuleuven.be'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from deconvoluted.transforms import fourier_transform, inverse_fourier_transform
